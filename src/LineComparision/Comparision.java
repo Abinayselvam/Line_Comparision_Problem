1d7581522941238dfc982700d9c90811280c5d45
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-11 17:35:45 +0530

    UC1:Find Line length

--- src/LineComparision/Comparision.java
+++ src/LineComparision/Comparision.java
@@ -2,5 +2,9 @@ public class Comparision {
 public static void main(String[] args)
 {
     System.out.println("Welcome to the Line Comparision Project");
+    //UC1 Calculate length of line
+
+    Line line=new Line(0,0,3,4);
+    System.out.println("Length of the Line: "+line.length());
 }
 }
---
+++ src/LineComparision/Line.java
@@ -0,0 +1,14 @@
+public class Line {
+    int x1,x2,y1,y2;
+    Line(int x1,int x2,int y1,int y2)
+    {
+        this.x1=x1;
+        this.x2=x2;
+        this.y1=y2;
+        this.y2=y2;
+    }
+    double length()
+    {
+        return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+    }
+}
