214baafaf45aa8bc9a2b3ef8967f38e3e39f1a01
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-12 16:14:42 +0530

    Day 8 Practice Problem

--- src/LineComparision/Comparision.java
+++ src/LineComparision/Comparision.java
@@ -1,27 +1,47 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class Comparision {
-public static void main(String[] args)
-{
-    System.out.println("Welcome to the Line Comparision Project");
-    //UC3 Compare the lines
+    public static void main(String[] args) {
 
-    Line line=new Line(0,0,3,4);
-    Line line1=new Line(0,0,6,8);
-    System.out.println("Length of the Line: "+line.length());
-    int result =line.compareTo(line1);
+        Scanner sc = new Scanner(System.in);
 
-    if (result == 0)
-        System.out.println("Lines are equal");
-    else if (result > 0)
-        System.out.println("Line1 is longer");
-    else
-        System.out.println("Line2 is longer");
+        System.out.println("Welcome to Line Comparison Computation Program");
 
-    if(line.equals(line1))
-    {
-        System.out.println("Lines are equal");
+        System.out.print("Enter Line 1 (x1 y1 x2 y2): ");
+        Line line1 = new Line(
+                new Point(sc.nextInt(), sc.nextInt()),
+                new Point(sc.nextInt(), sc.nextInt())
+        );
+
+        System.out.print("Enter Line 2 (x1 y1 x2 y2): ");
+        Line line2 = new Line(
+                new Point(sc.nextInt(), sc.nextInt()),
+                new Point(sc.nextInt(), sc.nextInt())
+        );
+
+        double len1 = line1.length();
+        double len2 = line2.length();
+
+        System.out.println("Length1 = " + len1);
+        System.out.println("Length2 = " + len2);
+
+        // UC2
+        System.out.println(line1.equals(line2)
+                ? "Lines are equal"
+                : "Lines are not equal");
+
+        // UC3
+        int result = line1.compareTo(line2);
+
+        if (result > 0) {
+            System.out.println("Line1 is longer");
+        } else if (result < 0) {
+            System.out.println("Line2 is longer");
+        } else {
+            System.out.println("Both lines are equal");
+        }
+
+        sc.close();
     }
-    else{
-        System.out.println("Lines are not equal");
-    }
-}
 }
--- src/LineComparision/Line.java
+++ src/LineComparision/Line.java
@@ -1,26 +1,31 @@
-public class Line implements Comparable<Line>{
-    int x1,x2,y1,y2;
-    Line(int x1,int x2,int y1,int y2)
-    {
-        this.x1=x1;
-        this.x2=x2;
-        this.y1=y1;
-        this.y2=y2;
-    }
-    double length()
-    {
-        return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+import java.awt.*;
+
+public class Line implements Comparable<Line> {
+
+    private final Point p1;
+    private final Point p2;
+
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
-    @Override
-    public int compareTo(Line other) {
-        return Double.compare(this.length(), other.length());
+    public double length() {
+        int dx = (int) (p2.getX() - p1.getX());
+        int dy = (int) (p2.getY() - p1.getY());
+        return Math.sqrt(dx * dx + dy * dy);
     }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (!(obj instanceof Line other)) return false;
 
         return Double.compare(this.length(), other.length()) == 0;
+    }
+
+    @Override
+    public int compareTo(Line other) {
+        return Double.compare(this.length(), other.length());
     }
 }
